53a14e312de735b3114b127d1aea7081d6a462df
aronunlabeled <xaron97x@hotmail.com>
2021-10-15 14:41:11 -0400

    saved

--- OOPProject/Library.cs
+++ OOPProject/Library.cs
@@ -6,27 +6,16 @@ namespace OOPProject
 {
     class Library
     {
-        List<Books> LibBooks = new List<Books>();
-
-        public Library(List<Books> LibBooks)
-        {
-            this.LibBooks = LibBooks;
-        }
-
-        public string FindName(List<Books> LibBooks)
-        {
-            int searchType = Validator.Validator.GetNumber("Type 1 to find book via number associated with it, or 2 to find it using all or part of the title", 1, 2);
-            if(searchType == 1)
-            {
-
-            }
-            
-        }
-        List<Books>   
-     
+        List<Books> BookList = new List<Books>();
+    
+        
+        
         
         public void display() {
-            
+            for (int i = 0; i < BookList.Count; i++)
+            {
+                Console.WriteLine(BookList[i].ToString());
+            }    
         }
 
     }
