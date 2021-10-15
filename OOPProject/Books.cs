94c82edcb5ad86d8bb1a5213bb84f6bdad989c26
Matt B <mjbeezer11621@gmail.com>
2021-10-15 12:55:57 -0400

    completed list of books

---
+++ OOPProject/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPProject
+{
+    class Books
+    {
+        //properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsCheckedOut { get; set; }
+
+        //constructor
+        public Books(string title, string author, bool ischeckedout)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.IsCheckedOut = ischeckedout;
+        }
+    }
+}
